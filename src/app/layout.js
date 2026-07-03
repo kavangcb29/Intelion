4a34e73c91c3a38d76045ee706c061b37daf4ec6
kavangcb29 <kavangcb@gmail.com>
2026-07-03 14:44:11 +0530

    Add Google AdSense script

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,10 +42,19 @@ export const metadata = {
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3571863616373313"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         <main className="main-content">
